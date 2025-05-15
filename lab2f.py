69e3259495576c51aee3b1fc0390de6bc6d5b6fd
Jeffrey Li <jli699@myseneca.ca>
2025-05-15 10:33:30 -0400

    added lab2f

---
+++ lab2f.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+
+#Author: Jeffrey Li
+#Author ID: jli699
+#Date Created: 2025/05/15
+
+timer = int(sys.argv[1]);
+
+while timer != 0:
+    print(timer);
+    timer -= 1;
+print('blast off!')
