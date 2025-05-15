56bd09e602b263b5532474724e567ef7c2a8c86c
Jeffrey Li <jli699@myseneca.ca>
2025-05-15 10:33:18 -0400

    added lab2e

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#Author: Jeffrey Li
+#Author ID: jli699
+#Date Created: 2025/05/15
+
+timer = 10
+
+while timer != 0:
+    print(timer);
+    timer -= 1;
+print('blast off!')
